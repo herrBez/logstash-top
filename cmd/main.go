528d586aefaace0c6a8ff4a449f8ff5ecea75914
Mirko Bez <mirko.bez@elastic.co>
2024-02-01 20:31:41 +0100

    Add possibility to stop querying Add keyboard actions to influence what it is displayed

--- cmd/main.go
+++ cmd/main.go
@@ -1,321 +1,128 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
 	"time"
 
-	"github.com/fatih/color"
+	logstash_flow "cmd/main.go/internal"
 
 	"atomicgo.dev/cursor"
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
 )
 
+var helpMessages = map[bool]string{
+	false: `Help Message
+- Up/Down to select the pipeline
+- Enter to select the pipeline
+- 'q' quit
+- 'p' pause stop querying new data
+`,
+	true: `Help Message
+- Up/Down to select the pipeline
+- Enter to select the pipeline
+- 'q' quit
+- 'b' back
+- 'p' pause stop querying new data
+`,
+}
+
+var pausedMessage = map[bool]string{
+	true:  "Active Query is stopped\n",
+	false: "\n",
+}
+
 func main() {
-	pipelineInfo := getPipelineInfo()
+	lfc := logstash_flow.NewLogstashFlowConfig("http://localhost:9600")
+	pipelineInfo := lfc.GetPipelineInfo()
+
+	pipelines := []string{}
+	selectedIndex := 0
+
+	for k, _ := range pipelineInfo.Pipelines {
+		pipelines = append(pipelines, k)
+	}
+
+	// Whether to quit the program or not
+	quit := false
+	// Whether a pipeline is selected or not
+	selected := false
+	// Whether we need to display the help message or the data
+	help := false
+	// Whether we perform curl request or we stop
+	paused := false
+
+	go keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		switch key.Code {
+		case keys.CtrlC:
+			quit = true
+			return true, nil // Stop listener
+		case keys.Down:
+			// if !selected {
+			selectedIndex = (selectedIndex + 1) % len(pipelines)
+			// }
+		case keys.Up:
+			// if !selected {
+			selectedIndex = (selectedIndex - 1 + len(pipelines)) % len(pipelines)
+			// }
+
+		case keys.Enter:
+			selected = true
+
+		case keys.RuneKey: // Check if key is a rune key (a, b, c, 1, 2, 3, ...)
+			if key.String() == "q" { // Check if key is "q"
+				quit = true
+				return true, nil // Stop listener
+			}
+			if key.String() == "b" {
+				if selected {
+					selected = false
+				}
+			}
+			if key.String() == "p" {
+				paused = !paused
+			}
+			if key.String() == "h" {
+				help = !help
+			}
+
+			// fmt.Printf("\rYou pressed the rune key: %s\n", key)
+			// default:
+			// 	fmt.Printf("\rYou pressed: %s\n", key)
+		}
+
+		return false, nil // Return false to continue listening
+	})
+
 	fmt.Println("Logstash Top")
 	fmt.Println("--------------------------")
 	area := cursor.NewArea()
-	header := "Pipeline\n"
-	area.Update(header)
+	pipelineFlowStatsAnswer := lfc.GetPipelineFlowStats(pipelineInfo)
 
-	origContent := header
-	for {
-		content := requestContent(pipelineInfo)
-		area.Update(origContent + content)
-		time.Sleep(1 * time.Second)
+	for !quit {
+		if help {
+			area.Update(helpMessages[selected])
+			time.Sleep(200 * time.Millisecond)
+		} else {
+			if !paused {
+				pipelineFlowStatsAnswer = lfc.GetPipelineFlowStats(pipelineInfo)
+			}
+			if !selected {
+				content := logstash_flow.RenderPipelineFlowStats(pipelineFlowStatsAnswer, pipelineInfo, pipelines, selectedIndex)
+
+				area.Update(content + pausedMessage[paused])
+				time.Sleep(1 * time.Second)
+
+			} else {
+
+				content := logstash_flow.RenderPipelineFlowStatsDetails(pipelineFlowStatsAnswer, pipelineInfo, pipelines, selectedIndex)
+
+				area.Update(content + pausedMessage[paused])
+				time.Sleep(1 * time.Second)
+			}
+		}
 	}
 
-}
+	area.Update("Quitting application\n\n")
 
-type FlowIntervals struct {
-	Current       float64 `json:"current"`
-	Last1Minute   float64 `json:"last_1_minute"`
-	Last5Minutes  float64 `json:"last_5_minutes"`
-	Last15Minutes float64 `json:"last_15_minutes"`
-	Last1Hour     float64 `json:"last_1_hour"`
-	Lifetime      float64 `json:"lifetime"`
-}
-
-type FlowWorkers struct {
-	WorkerUtilization    FlowIntervals `json:"worker_utilization"`
-	WorkerMillisPerEvent FlowIntervals `json:"worker_millis_per_event"`
-}
-
-type FilterOrOutput struct {
-	ID     string `json:"id"`
-	Name   string `json:"name"`
-	Events struct {
-		In               int `json:"in"`
-		Out              int `json:"out"`
-		DurationInMillis int `json:"duration_in_millis"`
-	} `json:"events"`
-	Flow FlowWorkers `json:"flow"`
-}
-
-type PipelineData struct {
-	Events struct {
-		QueuePushDurationInMillis int `json:"queue_push_duration_in_millis"`
-		In                        int `json:"in"`
-		Filtered                  int `json:"filtered"`
-		Out                       int `json:"out"`
-		DurationInMillis          int `json:"duration_in_millis"`
-	} `json:"events"`
-	Flow struct {
-		QueuePersistedGrowthEvents FlowIntervals `json:"queue_persisted_growth_events"`
-		QueuePersistedGrowthBytes  FlowIntervals `json:"queue_persisted_growth_bytes"`
-		OutputThroughput           FlowIntervals `json:"output_throughput"`
-		QueueBackpressure          FlowIntervals `json:"queue_backpressure"`
-		InputThroughput            FlowIntervals `json:"input_throughput"`
-		FilterThroughput           FlowIntervals `json:"filter_throughput"`
-		WorkerConcurrency          FlowIntervals `json:"worker_concurrency"`
-	} `json:"flow"`
-	Plugins struct {
-		Inputs []struct {
-			ID     string `json:"id"`
-			Name   string `json:"name"`
-			Events struct {
-				QueuePushDurationInMillis int `json:"queue_push_duration_in_millis"`
-				Out                       int `json:"out"`
-			} `json:"events"`
-			Flow struct {
-				Throughput FlowIntervals `json:"throughput"`
-			} `json:"flow"`
-		} `json:"inputs"`
-		Codecs []struct {
-			ID     string `json:"id"`
-			Name   string `json:"name"`
-			Encode struct {
-				WritesIn         int `json:"writes_in"`
-				DurationInMillis int `json:"duration_in_millis"`
-			} `json:"encode"`
-			Decode struct {
-				WritesIn         int `json:"writes_in"`
-				Out              int `json:"out"`
-				DurationInMillis int `json:"duration_in_millis"`
-			} `json:"decode"`
-		} `json:"codecs"`
-		Filters []FilterOrOutput `json:"filters"`
-		Outputs []FilterOrOutput `json:"outputs"`
-	} `json:"plugins"`
-	Reloads struct {
-		LastFailureTimestamp any `json:"last_failure_timestamp"`
-		Failures             int `json:"failures"`
-		LastSuccessTimestamp any `json:"last_success_timestamp"`
-		Successes            int `json:"successes"`
-		LastError            any `json:"last_error"`
-	} `json:"reloads"`
-	Queue struct {
-		Data struct {
-			Path             string `json:"path"`
-			FreeSpaceInBytes int64  `json:"free_space_in_bytes"`
-			StorageType      string `json:"storage_type"`
-		} `json:"data"`
-		Events   int `json:"events"`
-		Capacity struct {
-			MaxQueueSizeInBytes int `json:"max_queue_size_in_bytes"`
-			QueueSizeInBytes    int `json:"queue_size_in_bytes"`
-			MaxUnreadEvents     int `json:"max_unread_events"`
-			PageCapacityInBytes int `json:"page_capacity_in_bytes"`
-		} `json:"capacity"`
-		Type                string `json:"type"`
-		EventsCount         int    `json:"events_count"`
-		QueueSizeInBytes    int    `json:"queue_size_in_bytes"`
-		MaxQueueSizeInBytes int    `json:"max_queue_size_in_bytes"`
-	} `json:"queue"`
-	Hash        string `json:"hash"`
-	EphemeralID string `json:"ephemeral_id"`
-}
-
-// Generated with https://mholt.github.io/json-to-go/
-type PipelineAnswer struct {
-	Host        string `json:"host"`
-	Version     string `json:"version"`
-	HTTPAddress string `json:"http_address"`
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	EphemeralID string `json:"ephemeral_id"`
-	Status      string `json:"status"`
-	Snapshot    bool   `json:"snapshot"`
-	Pipeline    struct {
-		Workers    int `json:"workers"`
-		BatchSize  int `json:"batch_size"`
-		BatchDelay int `json:"batch_delay"`
-	} `json:"pipeline"`
-	Pipelines map[string]PipelineData `json:"pipelines"`
-}
-
-type PipelineConfiguration struct {
-	EphemeralID            string `json:"ephemeral_id"`
-	Hash                   string `json:"hash"`
-	Workers                int    `json:"workers"`
-	BatchSize              int    `json:"batch_size"`
-	BatchDelay             int    `json:"batch_delay"`
-	ConfigReloadAutomatic  bool   `json:"config_reload_automatic"`
-	ConfigReloadInterval   int64  `json:"config_reload_interval"`
-	DeadLetterQueueEnabled bool   `json:"dead_letter_queue_enabled"`
-}
-
-type NodeOverview struct {
-	Host        string `json:"host"`
-	Version     string `json:"version"`
-	HTTPAddress string `json:"http_address"`
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	EphemeralID string `json:"ephemeral_id"`
-	Status      string `json:"status"`
-	Snapshot    bool   `json:"snapshot"`
-	Pipeline    struct {
-		Workers    int `json:"workers"`
-		BatchSize  int `json:"batch_size"`
-		BatchDelay int `json:"batch_delay"`
-	} `json:"pipeline"`
-	Pipelines map[string]PipelineConfiguration `json:"pipelines"`
-	Os        struct {
-		Name                string `json:"name"`
-		Arch                string `json:"arch"`
-		Version             string `json:"version"`
-		AvailableProcessors int    `json:"available_processors"`
-	} `json:"os"`
-	Jvm struct {
-		Pid               int    `json:"pid"`
-		Version           string `json:"version"`
-		VMVersion         string `json:"vm_version"`
-		VMVendor          string `json:"vm_vendor"`
-		VMName            string `json:"vm_name"`
-		StartTimeInMillis int64  `json:"start_time_in_millis"`
-		Mem               struct {
-			HeapInitInBytes    int `json:"heap_init_in_bytes"`
-			HeapMaxInBytes     int `json:"heap_max_in_bytes"`
-			NonHeapInitInBytes int `json:"non_heap_init_in_bytes"`
-			NonHeapMaxInBytes  int `json:"non_heap_max_in_bytes"`
-		} `json:"mem"`
-		GcCollectors []string `json:"gc_collectors"`
-	} `json:"jvm"`
-}
-
-var yellow = color.New(color.FgYellow)
-var red = color.New(color.FgRed)
-var green = color.New(color.FgGreen)
-var blue = color.New(color.FgBlue)
-
-func getDiffString(diff float64) string {
-	diffOutput := ""
-	v := fmt.Sprintf("%8.4f", diff)
-	if diff < -0.1 {
-		diffOutput += red.Sprintf("%15s", v)
-	} else if diff > 0.1 {
-		diffOutput += green.Sprintf("%15s", v)
-	} else {
-		diffOutput += fmt.Sprintf("%15s", v)
-	}
-	return diffOutput
-}
-
-func printFlowWorkers(fw []FilterOrOutput) string {
-	output := fmt.Sprintf("+----------+---------------+---------------+---------------+---------------+\n")
-	output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|%-15s|\n", "Name", "CMillisPerEvent", "CurrentWU", "DiffLifetime", "Diff1Minutes")
-	output += fmt.Sprintf("+----------+---------------+---------------+---------------+---------------+\n")
-	for i := 0; i < len(fw); i++ {
-
-		CurrentWU := fmt.Sprintf("%f", fw[i].Flow.WorkerUtilization.Current)
-		CurrentWUMillis := fmt.Sprintf("%f", fw[i].Flow.WorkerMillisPerEvent.Current)
-
-		output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|%-15s|\n",
-			fw[i].Name,
-			CurrentWUMillis,
-			CurrentWU,
-			getDiffString(fw[i].Flow.WorkerUtilization.Current-
-				fw[i].Flow.WorkerUtilization.Lifetime),
-			getDiffString(fw[i].Flow.WorkerUtilization.Current-
-				fw[i].Flow.WorkerUtilization.Last1Minute),
-		)
-	}
-	output += fmt.Sprintf("+----------+---------------+---------------+---------------+---------------+\n")
-	return output
-
-}
-
-func getPipelineInfo() NodeOverview {
-	resp, err := http.Get("http://localhost:9600/_node")
-	if err != nil {
-		log.Panicf("Error %s", err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Panicf("Error %s", err)
-		os.Exit(1)
-	}
-	nodeOverview := NodeOverview{}
-	err = json.Unmarshal(body, &nodeOverview)
-
-	return nodeOverview
-
-}
-
-func requestContent(pipelineInfo NodeOverview) string {
-	resp, err := http.Get("http://localhost:9600/_node/stats/pipelines")
-	if err != nil {
-		log.Panicf("Error %s", err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Panicf("Error %s", err)
-		os.Exit(1)
-	}
-	output := ""
-
-	answer := PipelineAnswer{}
-	err = json.Unmarshal(body, &answer)
-	if err != nil {
-		log.Panicf("Could not unmarshal json %s", err)
-		os.Exit(1)
-	}
-	pipelineName := "test-http"
-
-	limit := min(len(body)-1, 10000)
-	output += fmt.Sprintf("%d %d\n", len(body), limit)
-	output += fmt.Sprintf("Workers: %d | Batch Size: %d | In: %d | Filtered: %d | Out %d \n",
-		pipelineInfo.Pipelines[pipelineName].Workers,
-		pipelineInfo.Pipelines[pipelineName].BatchSize,
-		answer.Pipelines[pipelineName].Events.In,
-		answer.Pipelines[pipelineName].Events.Filtered,
-		answer.Pipelines[pipelineName].Events.Out,
-	)
-
-	output += fmt.Sprintf("%30s", "===Inputs===\n")
-	output += fmt.Sprintf("+----------+---------------+---------------+---------------+\n")
-	output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|\n", "Name", "Current", "DiffLifetime", "Diff1Minutes")
-	output += fmt.Sprintf("+----------+---------------+---------------+---------------+\n")
-	for i := 0; i < len(answer.Pipelines[pipelineName].Plugins.Inputs); i++ {
-
-		Current := fmt.Sprintf("%f", answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Current)
-		output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|\n",
-			answer.Pipelines[pipelineName].Plugins.Inputs[i].Name,
-			Current,
-			getDiffString(answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Current-answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Lifetime),
-			getDiffString(answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Current-answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Last1Minute),
-		)
-	}
-	output += fmt.Sprintf("+----------+---------------+---------------+---------------+\n")
-
-	output += fmt.Sprintf("%30s", "===Filters===\n")
-	output += printFlowWorkers(answer.Pipelines[pipelineName].Plugins.Filters)
-
-	output += fmt.Sprintf("%30s", "===Outputs===\n")
-	output += printFlowWorkers(answer.Pipelines[pipelineName].Plugins.Outputs)
-
-	// output += fmt.Sprintf("%f", answer.Pipelines[pipelineName].Flow.QueuePersistedGrowthEvents.Last1Minute)
-
-	// for i := 0; i < limit; i += 100 {
-	// 	output += string(body[i:min(i+100, limit-1)]) + "\n"
-	// }
-	return output
 }
---
+++ internal/logstash_flow.go
@@ -0,0 +1,351 @@
+package logstash_flow
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+type LogstashFlowConfig struct {
+	Url string
+}
+
+func NewLogstashFlowConfig(url string) LogstashFlowConfig {
+	return LogstashFlowConfig{
+		Url: url,
+	}
+}
+
+type FlowIntervals struct {
+	Current       float64 `json:"current"`
+	Last1Minute   float64 `json:"last_1_minute"`
+	Last5Minutes  float64 `json:"last_5_minutes"`
+	Last15Minutes float64 `json:"last_15_minutes"`
+	Last1Hour     float64 `json:"last_1_hour"`
+	Lifetime      float64 `json:"lifetime"`
+}
+
+type FlowWorkers struct {
+	WorkerUtilization    FlowIntervals `json:"worker_utilization"`
+	WorkerMillisPerEvent FlowIntervals `json:"worker_millis_per_event"`
+}
+
+type FilterOrOutput struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Events struct {
+		In               int `json:"in"`
+		Out              int `json:"out"`
+		DurationInMillis int `json:"duration_in_millis"`
+	} `json:"events"`
+	Flow FlowWorkers `json:"flow"`
+}
+
+type Events struct {
+	QueuePushDurationInMillis int `json:"queue_push_duration_in_millis"`
+	In                        int `json:"in"`
+	Filtered                  int `json:"filtered"`
+	Out                       int `json:"out"`
+	DurationInMillis          int `json:"duration_in_millis"`
+}
+
+type PipelineData struct {
+	Events Events `json:"events"`
+	Flow   struct {
+		QueuePersistedGrowthEvents FlowIntervals `json:"queue_persisted_growth_events"`
+		QueuePersistedGrowthBytes  FlowIntervals `json:"queue_persisted_growth_bytes"`
+		OutputThroughput           FlowIntervals `json:"output_throughput"`
+		QueueBackpressure          FlowIntervals `json:"queue_backpressure"`
+		InputThroughput            FlowIntervals `json:"input_throughput"`
+		FilterThroughput           FlowIntervals `json:"filter_throughput"`
+		WorkerConcurrency          FlowIntervals `json:"worker_concurrency"`
+	} `json:"flow"`
+	Plugins struct {
+		Inputs []struct {
+			ID     string `json:"id"`
+			Name   string `json:"name"`
+			Events struct {
+				QueuePushDurationInMillis int `json:"queue_push_duration_in_millis"`
+				Out                       int `json:"out"`
+			} `json:"events"`
+			Flow struct {
+				Throughput FlowIntervals `json:"throughput"`
+			} `json:"flow"`
+		} `json:"inputs"`
+		Codecs []struct {
+			ID     string `json:"id"`
+			Name   string `json:"name"`
+			Encode struct {
+				WritesIn         int `json:"writes_in"`
+				DurationInMillis int `json:"duration_in_millis"`
+			} `json:"encode"`
+			Decode struct {
+				WritesIn         int `json:"writes_in"`
+				Out              int `json:"out"`
+				DurationInMillis int `json:"duration_in_millis"`
+			} `json:"decode"`
+		} `json:"codecs"`
+		Filters []FilterOrOutput `json:"filters"`
+		Outputs []FilterOrOutput `json:"outputs"`
+	} `json:"plugins"`
+	Reloads struct {
+		LastFailureTimestamp any `json:"last_failure_timestamp"`
+		Failures             int `json:"failures"`
+		LastSuccessTimestamp any `json:"last_success_timestamp"`
+		Successes            int `json:"successes"`
+		LastError            any `json:"last_error"`
+	} `json:"reloads"`
+	Queue struct {
+		Data struct {
+			Path             string `json:"path"`
+			FreeSpaceInBytes int64  `json:"free_space_in_bytes"`
+			StorageType      string `json:"storage_type"`
+		} `json:"data"`
+		Events   int `json:"events"`
+		Capacity struct {
+			MaxQueueSizeInBytes int `json:"max_queue_size_in_bytes"`
+			QueueSizeInBytes    int `json:"queue_size_in_bytes"`
+			MaxUnreadEvents     int `json:"max_unread_events"`
+			PageCapacityInBytes int `json:"page_capacity_in_bytes"`
+		} `json:"capacity"`
+		Type                string `json:"type"`
+		EventsCount         int    `json:"events_count"`
+		QueueSizeInBytes    int    `json:"queue_size_in_bytes"`
+		MaxQueueSizeInBytes int    `json:"max_queue_size_in_bytes"`
+	} `json:"queue"`
+	Hash        string `json:"hash"`
+	EphemeralID string `json:"ephemeral_id"`
+}
+
+// Generated with https://mholt.github.io/json-to-go/
+type PipelineAnswer struct {
+	Host        string `json:"host"`
+	Version     string `json:"version"`
+	HTTPAddress string `json:"http_address"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	EphemeralID string `json:"ephemeral_id"`
+	Status      string `json:"status"`
+	Snapshot    bool   `json:"snapshot"`
+	Pipeline    struct {
+		Workers    int `json:"workers"`
+		BatchSize  int `json:"batch_size"`
+		BatchDelay int `json:"batch_delay"`
+	} `json:"pipeline"`
+	Pipelines map[string]PipelineData `json:"pipelines"`
+}
+
+type PipelineConfiguration struct {
+	EphemeralID            string `json:"ephemeral_id"`
+	Hash                   string `json:"hash"`
+	Workers                int    `json:"workers"`
+	BatchSize              int    `json:"batch_size"`
+	BatchDelay             int    `json:"batch_delay"`
+	ConfigReloadAutomatic  bool   `json:"config_reload_automatic"`
+	ConfigReloadInterval   int64  `json:"config_reload_interval"`
+	DeadLetterQueueEnabled bool   `json:"dead_letter_queue_enabled"`
+}
+
+type NodeOverview struct {
+	Host        string `json:"host"`
+	Version     string `json:"version"`
+	HTTPAddress string `json:"http_address"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	EphemeralID string `json:"ephemeral_id"`
+	Status      string `json:"status"`
+	Snapshot    bool   `json:"snapshot"`
+	Pipeline    struct {
+		Workers    int `json:"workers"`
+		BatchSize  int `json:"batch_size"`
+		BatchDelay int `json:"batch_delay"`
+	} `json:"pipeline"`
+	Pipelines map[string]PipelineConfiguration `json:"pipelines"`
+	Os        struct {
+		Name                string `json:"name"`
+		Arch                string `json:"arch"`
+		Version             string `json:"version"`
+		AvailableProcessors int    `json:"available_processors"`
+	} `json:"os"`
+	Jvm struct {
+		Pid               int    `json:"pid"`
+		Version           string `json:"version"`
+		VMVersion         string `json:"vm_version"`
+		VMVendor          string `json:"vm_vendor"`
+		VMName            string `json:"vm_name"`
+		StartTimeInMillis int64  `json:"start_time_in_millis"`
+		Mem               struct {
+			HeapInitInBytes    int `json:"heap_init_in_bytes"`
+			HeapMaxInBytes     int `json:"heap_max_in_bytes"`
+			NonHeapInitInBytes int `json:"non_heap_init_in_bytes"`
+			NonHeapMaxInBytes  int `json:"non_heap_max_in_bytes"`
+		} `json:"mem"`
+		GcCollectors []string `json:"gc_collectors"`
+	} `json:"jvm"`
+}
+
+var yellow = color.New(color.FgYellow)
+var red = color.New(color.FgRed)
+var green = color.New(color.FgGreen)
+var blue = color.New(color.FgBlue)
+
+func getDiffString(diff float64) string {
+	diffOutput := ""
+	v := fmt.Sprintf("%8.4f", diff)
+	if diff < -0.1 {
+		diffOutput += red.Sprintf("%15s", v)
+	} else if diff > 0.1 {
+		diffOutput += green.Sprintf("%15s", v)
+	} else {
+		diffOutput += fmt.Sprintf("%15s", v)
+	}
+	return diffOutput
+}
+
+func printFlowWorkers(fw []FilterOrOutput) string {
+	output := fmt.Sprintf("+----------+---------------+---------------+---------------+---------------+\n")
+	output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|%-15s|\n", "Name", "CMillisPerEvent", "CurrentWU", "DiffLifetime", "Diff1Minutes")
+	output += fmt.Sprintf("+----------+---------------+---------------+---------------+---------------+\n")
+	for i := 0; i < len(fw); i++ {
+
+		CurrentWU := fmt.Sprintf("%f", fw[i].Flow.WorkerUtilization.Current)
+		CurrentWUMillis := fmt.Sprintf("%f", fw[i].Flow.WorkerMillisPerEvent.Current)
+
+		output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|%-15s|\n",
+			fw[i].Name,
+			CurrentWUMillis,
+			CurrentWU,
+			getDiffString(fw[i].Flow.WorkerUtilization.Current-
+				fw[i].Flow.WorkerUtilization.Lifetime),
+			getDiffString(fw[i].Flow.WorkerUtilization.Current-
+				fw[i].Flow.WorkerUtilization.Last1Minute),
+		)
+	}
+	output += fmt.Sprintf("+----------+---------------+---------------+---------------+---------------+\n")
+	return output
+
+}
+
+func (lfc LogstashFlowConfig) GetPipelineInfo() NodeOverview {
+	resp, err := http.Get(lfc.Url + "/_node")
+	if err != nil {
+		log.Panicf("Error %s", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicf("Error %s", err)
+		os.Exit(1)
+	}
+	nodeOverview := NodeOverview{}
+	err = json.Unmarshal(body, &nodeOverview)
+
+	return nodeOverview
+}
+
+func (lfc LogstashFlowConfig) GetPipelineFlowStats(pipelineInfo NodeOverview) PipelineAnswer {
+	resp, err := http.Get(lfc.Url + "/_node/stats/pipelines")
+	if err != nil {
+		log.Panicf("Error %s", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicf("Error %s", err)
+		os.Exit(1)
+	}
+
+	answer := PipelineAnswer{}
+	err = json.Unmarshal(body, &answer)
+	if err != nil {
+		log.Panicf("Could not unmarshal json %s", err)
+		os.Exit(1)
+	}
+	return answer
+}
+
+func RenderPipelineFlowStatsDetails(answer PipelineAnswer, pipelineInfo NodeOverview, pipelines []string, selectedIndex int) string {
+	output := ""
+	pipelineName := pipelines[selectedIndex]
+	output += fmt.Sprintf("Pipeline '%s'\n", pipelineName)
+	output += fmt.Sprintf("Queue '%s'\n", answer.Pipelines[pipelineName].Queue.Type)
+
+	output += fmt.Sprintf("Workers: %d | Batch Size: %d | In: %d | Filtered: %d | Out %d \n",
+		pipelineInfo.Pipelines[pipelineName].Workers,
+		pipelineInfo.Pipelines[pipelineName].BatchSize,
+		answer.Pipelines[pipelineName].Events.In,
+		answer.Pipelines[pipelineName].Events.Filtered,
+		answer.Pipelines[pipelineName].Events.Out,
+	)
+
+	output += fmt.Sprintf("%30s", "===Inputs===\n")
+	output += fmt.Sprintf("+----------+---------------+---------------+---------------+\n")
+	output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|\n", "Name", "Current", "DiffLifetime", "Diff1Minutes")
+	output += fmt.Sprintf("+----------+---------------+---------------+---------------+\n")
+	for i := 0; i < len(answer.Pipelines[pipelineName].Plugins.Inputs); i++ {
+
+		Current := fmt.Sprintf("%f", answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Current)
+		output += fmt.Sprintf("|%-10s|%-15s|%-15s|%-15s|\n",
+			answer.Pipelines[pipelineName].Plugins.Inputs[i].Name,
+			Current,
+			getDiffString(answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Current-answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Lifetime),
+			getDiffString(answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Current-answer.Pipelines[pipelineName].Plugins.Inputs[i].Flow.Throughput.Last1Minute),
+		)
+	}
+	output += fmt.Sprintf("+----------+---------------+---------------+---------------+\n")
+
+	output += fmt.Sprintf("%30s", "===Filters===\n")
+	output += printFlowWorkers(answer.Pipelines[pipelineName].Plugins.Filters)
+
+	output += fmt.Sprintf("%30s", "===Outputs===\n")
+	output += printFlowWorkers(answer.Pipelines[pipelineName].Plugins.Outputs)
+
+	// output += fmt.Sprintf("%f", answer.Pipelines[pipelineName].Flow.QueuePersistedGrowthEvents.Last1Minute)
+
+	// for i := 0; i < limit; i += 100 {
+	// 	output += string(body[i:min(i+100, limit-1)]) + "\n"
+	// }
+	return output
+}
+
+func FloatString(v float64) string {
+	return fmt.Sprintf("%.4f", v)
+}
+
+func RenderPipelineFlowStats(answer PipelineAnswer, pipelineInfo NodeOverview, pipelines []string, selectedIndex int) string {
+	output := ""
+	padding := "  "
+
+	cumulativeEvent := Events{}
+	output += fmt.Sprintf("%s+---------------+---------------+---------------+---------------+---------------+---------------+\n", padding)
+
+	output += fmt.Sprintf("%s|%-15s|%15s|%15s|%15s|%15s|%15s|\n",
+		padding, "Pipeline", "Input", "Filter", "Output", "QueueBackpress", "WorkerConcurren")
+
+	output += fmt.Sprintf("%s+---------------+---------------+---------------+---------------+---------------+---------------+\n", padding)
+
+	for i := range pipelines {
+		ithData := answer.Pipelines[pipelines[i]]
+		if selectedIndex == i {
+			output += fmt.Sprintf("->")
+		} else {
+			output += fmt.Sprintf("  ")
+		}
+		cumulativeEvent.In += ithData.Events.In
+		cumulativeEvent.Filtered += ithData.Events.Filtered
+		cumulativeEvent.Out += ithData.Events.Out
+		output += fmt.Sprintf("|%-15s|%15d|%15d|%15d|%15s|%15s|\n", pipelines[i], ithData.Events.In, ithData.Events.Filtered, ithData.Events.Out, FloatString(ithData.Flow.QueueBackpressure.Current), FloatString(ithData.Flow.WorkerConcurrency.Current))
+	}
+	output += fmt.Sprintf("%s+---------------+---------------+---------------+---------------+---------------+---------------+\n", padding)
+	output += fmt.Sprintf("%s|%-15s|%15d|%15d|%15d|%15s|%15s|\n", padding, "total", cumulativeEvent.In, cumulativeEvent.Filtered, cumulativeEvent.Out, "N/A", "N/A")
+	output += fmt.Sprintf("%s+---------------+---------------+---------------+---------------+---------------+---------------+\n", padding)
+
+	return output
+}
